7b7929cbd43856daedd7df2aaff5e401c4b2abc0
StrRst <42658552+StrRst@users.noreply.github.com>
2020-07-10 02:25:00 +0300

    Move MainActivity to "activity" folder

--- app/src/main/java/com/example/sampleapp/activity/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.sampleapp;
+package com.example.sampleapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.sampleapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
