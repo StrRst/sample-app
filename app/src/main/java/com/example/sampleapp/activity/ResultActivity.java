49d209a55c468b5cc661a4b8b51d35e85f7b2a07
StrRst <42658552+StrRst@users.noreply.github.com>
2020-07-10 06:38:19 +0300

    Implement displaying of received text

--- app/src/main/java/com/example/sampleapp/activity/ResultActivity.java
+++ app/src/main/java/com/example/sampleapp/activity/ResultActivity.java
@@ -1,16 +1,38 @@
 package com.example.sampleapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import android.os.Bundle;
 
 import com.example.sampleapp.R;
+import com.example.sampleapp.utils.Constants;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private AppCompatTextView receivedText;
+    private AppCompatButton okButton;
+    private AppCompatButton cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        receivedText = findViewById(R.id.received_text);
+        okButton = findViewById(R.id.ok_btn);
+        cancelButton = findViewById(R.id.cancel_btn);
+
+        setReceivedText();
+    }
+
+    private void setReceivedText() {
+        if (getIntent().getExtras() != null) {
+            String receivedData = getIntent().getStringExtra(Constants.EXTRA_DATA);
+            if (receivedData != null) {
+                receivedText.setText(receivedData);
+            }
+        }
     }
 }
