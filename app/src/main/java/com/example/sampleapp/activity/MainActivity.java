1882a92d3b6902069ccc261b935e0a565597dccf
StrRst <42658552+StrRst@users.noreply.github.com>
2020-07-10 04:27:07 +0300

    Add handler for clicking input confirmation button

--- app/src/main/java/com/example/sampleapp/activity/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/activity/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.sampleapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.sampleapp.utils.Constants;
 import com.example.sampleapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatEditText inputField;
+    private AppCompatButton confirmInputButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputField = findViewById(R.id.input_field);
+        confirmInputButton = findViewById(R.id.confirm_input_btn);
+
+        confirmInputButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(inputField.getText().toString())) {
+                    showToast("Error: input field is empty");
+                } else {
+                    startResultActivity();
+                }
+            }
+        });
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
+    }
+
+    private void startResultActivity() {
+        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+        intent.putExtra(Constants.EXTRA_DATA, inputField.getText().toString());
+        startActivityForResult(intent, Constants.REQUEST_CODE);
     }
 }
---
+++ app/src/main/java/com/example/sampleapp/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.sampleapp.utils;
+
+public class Constants {
+
+    public final static int REQUEST_CODE = 111;
+
+    public final static String EXTRA_DATA = "com.keepsolid.ksinternshipdemo2020.EXTRA_DATA";
+}
